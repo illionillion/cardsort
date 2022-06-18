09a5f61945be132ce27e00e67a4ab8011e10c834
yuto ichinose <yuto.0526.sano@gmail.com>
2022-06-18 18:01:23 +0900

    first commit

---
+++ main.mjs
@@ -0,0 +1,24 @@
+'use strict'
+import getLength from "./lib/getLength.mjs";
+// const list = process.argv.slice(2)
+const list = ['a', 'b', 'c']
+console.log(list);
+console.log(list.length);
+
+// 全パターン並び替え
+const resultlist = new Array(getLength(list.length)).fill([]) // 格納用
+console.log(resultlist.length);
+
+let count = 0
+while (count < resultlist.length) {
+    const result = new Array(list.length).fill('')
+    for (let a = 0; a < list.length; a++) {
+        // 1個目 (aから時まる)
+        result[a] = list[a]
+    }
+    
+    resultlist[count++] = result
+}
+
+
+console.log(resultlist);
